b851c24fe1964b1e1bd586cc05bf7b71f3578c85
sanskarOH <sanskaryeeha@gmail.com>
2026-04-24 19:05:21 +0530

    feat:added healthcheck route

--- server/app.js
+++ server/app.js
@@ -2,10 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const bfhlRoutes = require("./routes/bfhl.routess");
 const path = require('path');
+const { timeStamp } = require("console");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "ok",
+        uptime: process.uptime(),
+        timeStamp: new Date().toISOString(),
+    })
+})
+
 app.use("/bfhl", bfhlRoutes);
 
 app.use(express.static(path.join(__dirname,"./public")));
@@ -13,4 +23,6 @@ app.use(express.static(path.join(__dirname,"./public")));
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"))
 })
+
+
 module.exports = app;
